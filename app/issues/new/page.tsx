a7f8c9736ee8d64d9fc5df97b231afe114e327fb
Anastasia <555.555.anastasia@gmail.com>
2024-04-06 12:07:26 +0700

    validation CreateIssueForm

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,64 +1,111 @@
 "use client";
 
-import React, {useMemo} from 'react';
-import dynamic from 'next/dynamic';
-const SimpleMDE = dynamic(() => import('react-simplemde-editor'), { ssr: false })
+import React, { useMemo, useState } from "react";
+import dynamic from "next/dynamic";
+const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
+  ssr: false,
+});
 import "easymde/dist/easymde.min.css";
-import axios from 'axios'
-import { useRouter } from 'next/navigation'
 
-import { useForm, Controller } from "react-hook-form"
+import { IssueForm } from "@/app/validation-schemas";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { useForm, Controller } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { createIssueSchema } from "@/app/validation-schemas";
 
-interface IssueForm {
-  title: string;
-  description: string;
-}
+import ErrorMessage from "@/app/components/error-message/error-message";
 
 const NewIssuePage = () => {
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema),
+  });
 
-  const { register, control, handleSubmit } = useForm<IssueForm>();
+  const [error, setError] = useState<string | null>(null);
 
   const mdeOptions = useMemo(() => {
     return {
       autofocus: true,
-      spellChecker: false
-    }
+      spellChecker: false,
+    };
   }, []);
 
-
-  const router = useRouter()
+  const router = useRouter();
 
   return (
-    <div className='flex items-center justify-center'>
-      <form className="max-w-xl space-y-5" onSubmit={handleSubmit( async (data) => 
-      {
-        await axios.post('/api/issues', data);
-        router.push('/issues');
-      })}>
+    <>
+      <div className="flex items-center justify-center h-full">
+        <form
+          className="max-w-xl space-y-5"
+          onSubmit={handleSubmit(async (data) => {
+            try {
+              await axios.post("/api/issues", data);
+              router.push("/issues");
+            } catch (error) {
+              setError("Unaxpected error. Please try again later.");
+            }
+          })}
+        >
+          <h1 className="text-3xl text-center text-zinc-600 font-bold">
+            New Issue
+          </h1>
 
-        <h1 className="text-3xl text-center text-zinc-600 font-bold">New Issue</h1>
+          <input
+            type="text"
+            {...register("title")}
+            placeholder="Title"
+            className="input input-bordered w-full"
+          />
+          <ErrorMessage>{errors.title?.message}</ErrorMessage>
 
-        <input type="text" {...register("title")} placeholder="Title" className="input input-bordered w-full" />
+          <Controller
+            control={control}
+            name="description"
+            render={({ field: { ref, value, onBlur, onChange } }) => (
+              <SimpleMDE
+                value={value}
+                onChange={onChange}
+                onBlur={onBlur}
+                className="textarea textarea-bordered"
+                placeholder="Description"
+                options={mdeOptions}
+              />
+            )}
+          />
 
-        <Controller
-          control={control}
-          name="description"
-          render={({ field: { ref, value, onBlur, onChange } }) => (
-            <SimpleMDE
-              value={value}
-              onChange={onChange}
-              onBlur={onBlur}
-              className="textarea textarea-bordered"
-              placeholder="Description"
-              options={mdeOptions}
-            />
+       
+            <ErrorMessage>{errors.description?.message}</ErrorMessage>
+        
+          {error && (
+            <div role="alert" className="alert alert-error">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="stroke-current shrink-0 h-6 w-6 display-flex justify-end items-end"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
+              <span>{error}</span>
+            </div>
           )}
-        />
+          <button type="submit" className="btn btn-neutral w-full">
+            Submit
+          </button>
+        </form>
+      </div>
+    </>
+  );
+};
 
-        <button type='submit' className="btn btn-neutral w-full">Submit</button>
-      </form>
-    </div>
-  )
-}
-
-export default NewIssuePage
+export default NewIssuePage;
---
+++ app/validation-schemas.ts
@@ -0,0 +1,11 @@
+import { z } from "zod";
+
+// Раньше мы называли объект для валидации schema, но в больших
+// проектах у нас может быть множество схем для разных задач.
+// Поэтому назовем ее более осмысленно.
+export const createIssueSchema = z.object({
+  title: z.string({ required_error: "Title is required" }).min(1, "Title must be at least 1 character").max(255),
+  description: z.string({ required_error: "Description is required" }).min(1, "Description must be at least 1 character")
+});
+
+export type IssueForm = z.infer<typeof createIssueSchema>;
